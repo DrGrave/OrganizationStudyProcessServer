322f66da5d6b11fadd97bfbae0c00c1a7dd4c00e
vadim <Vadim3111@tut.by>
2017-06-18 16:13:45 +0300

    Configure project.

---
+++ src/main/java/com/vkkzlabs/SystemoforganizationstufyprocessApplication.java
@@ -0,0 +1,12 @@
+package com.vkkzlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemoforganizationstufyprocessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemoforganizationstufyprocessApplication.class, args);
+	}
+}
