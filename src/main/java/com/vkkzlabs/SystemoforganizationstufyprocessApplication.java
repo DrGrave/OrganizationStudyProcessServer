d54fe14c25b75764f511810192ec192abfe4c608
Vadim Vidnichuk <VadimXPZL@gmail.com>
2017-06-20 21:40:17 +0300

    Fixes.

--- src/main/java/com/vkkzlabs/SystemoforganizationstufyprocessApplication.java
+++ src/main/java/com/vkkzlabs/SystemoforganizationstufyprocessApplication.java
@@ -3,6 +3,8 @@ package com.vkkzlabs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 @EnableAutoConfiguration
 @SpringBootApplication
 public class SystemoforganizationstufyprocessApplication {
