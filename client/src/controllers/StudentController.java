843250f8753e373b07b7938ab248edebfe16a86e
vadim <Vadim3111@tut.by>
2017-07-24 01:01:12 +0300

    Do sum fixes of transfer date.

--- client/src/controllers/StudentController.java
+++ client/src/controllers/StudentController.java
@@ -1,11 +1,14 @@
 package controllers;
 import com.vkkzlabs.entity.MyUser;
 import com.vkkzlabs.entity.MyUserCredentials;
+import controllers.studentsControllers.ButtonBoxForDebtController;
+import controllers.studentsControllers.TableOfDebitsController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
@@ -64,18 +67,18 @@ public class StudentController {
     }
 
     public void initialize() throws IOException {
-//        userName.setText(iUser.getUserName());
-//        userSurname.setText(iUser.getUserSurname());
-//        userPatronymic.setText(iUser.getUserPatronymic());
-//        userGender.setText(iUser.getGender().getNameOfGender());
-//        userGroup.setText(iUser.getStudentGroup().getNumberOfGroup());
-//        userEmail.setText(iUser.getEmail());
-//        userType.setText(iUser.getUserType().getNameUserType());
-        Parent debtTable = FXMLLoader.load(getClass().getResource("../samples/studentFXML/DebtTable.fxml"));
+
+        FXMLLoader debtTable = new  FXMLLoader(getClass().getResource("../samples/studentFXML/DebtTable.fxml"));
+        TableOfDebitsController tableOfDebitsController = new TableOfDebitsController();
+        debtTable.setController(tableOfDebitsController);
+        firstSudentInfoBox.getChildren().add(debtTable.load());
+
         
-        Parent buttonBoxForDebtTable = FXMLLoader.load(getClass().getResource("../samples/studentFXML/ButtonBoxForDebtTable.fxml"));
-        firstSudentInfoBox.getChildren().add(debtTable);
-        firstSudentInfoBox.getChildren().add(buttonBoxForDebtTable);
+        FXMLLoader buttonBoxForDebtTable =new  FXMLLoader(getClass().getResource("../samples/studentFXML/ButtonBoxForDebtTable.fxml"));
+        ButtonBoxForDebtController buttonBoxForDebtController = new ButtonBoxForDebtController();
+        buttonBoxForDebtTable.setController(buttonBoxForDebtController);
+        firstSudentInfoBox.getChildren().add(buttonBoxForDebtTable.load());
+
 
         checkForBadStudent(iUser);
         checkForAchievement(iUser);
