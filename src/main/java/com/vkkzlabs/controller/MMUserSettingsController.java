fdeecfd423e494eec4d3c719d7ab1a1da4776e40
Vadim Vidnichuk <VadimXPZL@gmail.com>
2017-07-25 22:00:08 +0300

    Fix

--- src/main/java/com/vkkzlabs/controller/MMUserSettingsController.java
+++ src/main/java/com/vkkzlabs/controller/MMUserSettingsController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(name = "MMUserSettings")
+@RequestMapping("MMUserSettings")
 public class MMUserSettingsController {
     @Autowired
     private MMUserSettingsService mmUserSettingsService;
